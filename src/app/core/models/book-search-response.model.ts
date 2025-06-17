2272897114636ecd1a26175619fce0f89426c33a
eriksgda <erik.ksgda@gmail.com>
2025-06-16 22:12:42 -0300

    feat: add book-search-response and raw-book models

---
+++ src/app/core/models/book-search-response.model.ts
@@ -0,0 +1,5 @@
+import {RawBookModel} from './raw-book.model';
+
+export interface BookSearchResponse {
+  docs: RawBookModel[];
+}
---
+++ src/app/core/models/raw-book.model.ts
@@ -0,0 +1,8 @@
+export interface RawBookModel {
+  title: string;
+  key: string;
+  author_key: string[];
+  author_name: string[];
+  cover_i?: string;
+  first_publish_year?: number;
+}
