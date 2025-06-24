d0e672e0d5b004f1168cfee021cc84d1cee55c21
eriksgda <erik.ksgda@gmail.com>
2025-06-23 23:16:38 -0300

    feat: add pagination component

---
+++ src/app/features/home/components/pagination/pagination.component.ts
@@ -0,0 +1,44 @@
+import {Component, EventEmitter, Input, Output, signal} from '@angular/core';
+
+@Component({
+  selector: 'app-pagination',
+  imports: [],
+  templateUrl: './pagination.component.html',
+  styleUrl: './pagination.component.scss'
+})
+export class PaginationComponent {
+
+  protected MAX_PAGE: number = 20;
+
+  @Output() pageChanged = new EventEmitter<number>();
+  @Input() public currentPage = signal(1);
+
+  nextPage() {
+    if (this.currentPage() <= this.MAX_PAGE) {
+      this.currentPage.update(value => value + 1);
+
+      this.pageChanged.emit(this.currentPage());
+    }
+  }
+
+  lastPage() {
+    this.currentPage.set(this.MAX_PAGE);
+
+    this.pageChanged.emit(this.currentPage());
+  }
+
+  prevPage() {
+    if (this.currentPage() > 1) {
+      this.currentPage.update(value => value - 1);
+
+      this.pageChanged.emit(this.currentPage());
+    }
+  }
+
+  firstPage() {
+    this.currentPage.set(1);
+
+    this.pageChanged.emit(this.currentPage());
+  }
+
+}
