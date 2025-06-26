c2dbf57d862a6a4f7d76c225e963b0b129e00078
eriksgda <erik.ksgda@gmail.com>
2025-06-25 21:11:59 -0300

    feat: implement methods for localStorage management

--- src/app/infrastructure/storage/wishlist.service.ts
+++ src/app/infrastructure/storage/wishlist.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import {WishlistItemsModel} from '../../core/models/wishlist-items.model';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,28 @@ import { Injectable } from '@angular/core';
 export class WishlistService {
 
   constructor() { }
+
+  addBookToWishlist(wishlistItem: WishlistItemsModel) {
+    const wishlist = JSON.parse(localStorage.getItem("wishlist") || "[]");
+
+    if (!wishlist.includes(wishlistItem)) {
+      wishlist.push(wishlistItem);
+      localStorage.setItem("wishlist", JSON.stringify(wishlist));
+    }
+  }
+
+  removeBookFromWishlist(wishlistItem: WishlistItemsModel) {
+    let wishlist = JSON.parse(localStorage.getItem("wishlist") || "[]");
+    wishlist = wishlist.filter((value: WishlistItemsModel) => value !== wishlistItem);
+    localStorage.setItem("wishlist", JSON.stringify(wishlist));
+  }
+
+  isInWishlist(wishlistItem: WishlistItemsModel) {
+    const wishlist = JSON.parse(localStorage.getItem("wishlist") || "[]");
+    return wishlist.includes(wishlistItem);
+  }
+
+  getAllBooks(): WishlistItemsModel[] {
+    return JSON.parse(localStorage.getItem("wishlist") || "[]");
+  }
 }
