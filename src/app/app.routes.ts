a69815c4bc5ce83f95f66316a74db7885cbf2598
eriksgda <erik.ksgda@gmail.com>
2025-06-25 21:13:36 -0300

    feat: add wishlist path in routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from './features/home/home.component';
+import {WishlistComponent} from './features/wishlist/wishlist.component';
 
 export const routes: Routes = [
   {
     path: "home",
     component: HomeComponent,
+  },
+  {
+    path: "wishlist",
+    component: WishlistComponent,
   },
   {
     path: "",
