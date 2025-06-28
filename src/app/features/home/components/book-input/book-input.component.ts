ddb4a0c3b4de71272a1bd6c275d4c9dd278fd8eb
eriksgda <erik.ksgda@gmail.com>
2025-06-27 23:30:19 -0300

    feat: add select options in form and Cat button

--- src/app/features/home/components/book-input/book-input.component.ts
+++ src/app/features/home/components/book-input/book-input.component.ts
@@ -1,4 +1,6 @@
 import {Component, computed, EventEmitter, Output, signal} from '@angular/core';
+import {SearchType} from '../../../../core/models/search-type.enum';
+import {CatSearch} from '../../../../core/models/cat.enum';
 
 @Component({
   selector: 'app-book-input',
@@ -10,14 +12,32 @@ export class BookInputComponent {
   public title = 'library';
 
   public bookQuery = signal("");
+  searchType = signal<SearchType>(SearchType.Title);
 
   isSearchDisabled = computed(() => this.bookQuery().trim() === '');
 
-  @Output() public valueSubmit = new EventEmitter<string>();
+  @Output()
+  public valueSubmit = new EventEmitter<{ query: string, type: SearchType }>();
 
   public onSubmit(event: Event): void {
     event.preventDefault();
-    this.valueSubmit.emit(this.bookQuery());
+
+    if (this.isClicked) {
+      this.bookQuery.set(this.getRandomCatSearch());
+    }
+    this.valueSubmit.emit({query: this.bookQuery(), type: this.searchType()});
     this.bookQuery.set('');
+  }
+
+  isClicked: boolean = false;
+
+  public toggleCatButton() {
+    this.isClicked = !this.isClicked;
+  }
+
+  private getRandomCatSearch(): CatSearch {
+    const values = Object.values(CatSearch);
+    const randomIndex = Math.floor(Math.random() * values.length);
+    return values[randomIndex];
   }
 }
