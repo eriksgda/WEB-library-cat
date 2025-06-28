f64b903736230c6df2784e77e3deadcfd9d7f4bc
eriksgda <erik.ksgda@gmail.com>
2025-06-27 23:31:01 -0300

    feat: add cat button enums

---
+++ src/app/core/models/cat.enum.ts
@@ -0,0 +1,15 @@
+export enum CatSearch {
+  cat = "cat",
+  meow = "meow",
+  purr = "purr",
+  kitty = "kitty",
+  car = "car",
+  gato = "gato",
+  miau = "miau",
+  peixe = "peixe",
+  fish = "fish",
+  rato = "rato",
+  mouse = "mouse",
+  rat = "rat",
+  pussy = "pussy"
+}
