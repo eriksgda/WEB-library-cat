2dc6231c74c87bad0af01b66d641569bd5fcbb1a
eriksgda <erik.ksgda@gmail.com>
2025-07-03 19:49:06 -0300

    feat: add responsiveness to header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,22 +1,38 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterLinkActive} from "@angular/router";
 import {TranslatePipe, TranslateService} from '@ngx-translate/core';
+import {LucideAngularModule} from 'lucide-angular';
 
 @Component({
   selector: 'app-header',
   imports: [
     RouterLink,
     RouterLinkActive,
-    TranslatePipe
+    TranslatePipe,
+    LucideAngularModule
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  constructor(private translate: TranslateService) { }
+  constructor(private translate: TranslateService) {
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
+  }
 
   toggleLanguage() {
     this.translate.use(this.translate.currentLang === 'en' ? 'pt' : 'en');
   }
 
+  isMenuOpen: boolean = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
+
+  protected readonly window = window;
 }
